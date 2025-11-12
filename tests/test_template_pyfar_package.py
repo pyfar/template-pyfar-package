b52e24308da3f36338c8d7666f87fdd738f92d36
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-11-12 15:55:55 +0100

    add tests

--- tests/test_template_pyfar_package.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-def test_import_template_pyfar_package():
-    try:
-        import template_pyfar_package           # noqa
-    except ImportError:
-        pytest.fail('import template_pyfar_package failed')
