2d63f1cff045617a460f070592745994d55aba16
Anne Heimes <ahe@akustik.rwth-aachen.de>
2026-04-11 12:18:56 +0200

    add action to create pr and test

--- tests/test_copier.py
+++ tests/test_copier.py
@@ -35,6 +35,7 @@ def test_project_folder(default_project):
     ".gitignore",
     ".circleci/config.yml",
     ".github/workflows/add_to_project.yml",
+    ".github/workflows/create_pr_on_template_updates.yml",
 ])
 def test_generated_file_exists(default_project, file_name):
     assert default_project.project_dir.joinpath(file_name).exists()
@@ -229,3 +230,13 @@ def test_content_MANIFEST(default_project, desired):
     content = default_project.project_dir.joinpath(
                                     "MANIFEST.in").read_text()
     assert desired in content
+
+
+@pytest.mark.parametrize("desired", [
+    '          python-version: 3.14',
+])
+def test_content_github_action_create_pr_on_template_updates(default_project, desired):
+    content = default_project.project_dir.joinpath(
+        ".github/workflows/create_pr_on_template_updates.yml").read_text()
+    assert desired in content
+
