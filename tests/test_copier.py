8837d9302a84025fffb6905d167a7eb0a9c0dd5f
Anne Heimes <ahe@akustik.rwth-aachen.de>
2026-05-10 10:50:36 +0200

    fix ruff

--- tests/test_copier.py
+++ tests/test_copier.py
@@ -235,7 +235,8 @@ def test_content_MANIFEST(default_project, desired):
 @pytest.mark.parametrize("desired", [
     '          python-version: "3.14"',
 ])
-def test_content_github_action_create_pr_on_template_updates(default_project, desired):
+def test_content_github_action_create_pr_on_template_updates(
+        default_project, desired):
     content = default_project.project_dir.joinpath(
         ".github/workflows/create_pr_on_template_updates.yml").read_text()
     assert desired in content
